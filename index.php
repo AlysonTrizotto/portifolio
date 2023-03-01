9509a05555a6cd4cc14268f279a9a04db051e5fb
Alyson Trizotto <79091765+AlysonTrizotto@users.noreply.github.com>
2023-03-01 10:54:31 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+declare(strict_types=1);
+
+require_once __DIR__ . './src/vendor/autoload.php';
+
+require_once "./src/view/home.php";
+
+?>
---
+++ src/view/home.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="../../src/view/home.css">
+	<title>Alyson Trizotto</title>
+</head>
+<body>
+  <nav id="nav">
+    <a class="menu" href="#">Home</a>
+    <a class="menu" href="#">Sobre</a>
+    <a class="menu" href="#">Blog</a>
+    <a class="menu" href="#">Contato</a>
+  </nav>
+
+</body>
+</html>
